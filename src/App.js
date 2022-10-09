94fa419410b64fdb2e005d395eb926233520d50d
YellowDuckcument <109415258+YellowDuckcument@users.noreply.github.com>
2022-10-10 00:10:24 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import BoardView from './component/BoardView';
+import './main.scss'
+import './styles.scss'
+
+function App() {
+  return (
+    <div>
+      <BoardView />
+    </div>
+  )
+}
+
+export default App;
